0cc21f38f017348af8013e7be2b40c5e3d23672a
bilal <bilalalam173@gmail.com>
2020-12-16 18:41:23 +0500

    v 1.0 complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
+import { white } from "chalk"
 import * as React from "react"
 
 // styles
 const pageStyles = {
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  font: 'Arial',
 }
 const navBar = {
   height: '100px',
@@ -27,6 +28,121 @@ const menuItem = {
   display: 'inline',
   color: 'white',
   fontSize: '20px',
+  paddingRight: '20px'
+}
+
+
+const greeting = {
+  fontSize: '70px',
+  fontWeight: '200',
+  color: 'rgb(51,51,51)',
+  lineHeight: '70px',
+  outlineColor: 'rgb(51,51,51)',
+  display: 'block'
+}
+
+const hiImage = {
+  height: '100px',
+  width: '100px'
+}
+const name = {
+  fontWeight: '400',
+  color: '#009688'
+}
+
+const description = {
+  fontSize: '30px',
+  fontWeight: '200',
+  maxWidth: '600px',
+  lineHeight: '50px',
+  textAlign: 'left'
+}
+
+const teal = {
+  color: '#009688',
+  fontWeight: '500'
+}
+
+const highlight = {
+  // color: '#009688',
+  fontWeight: '600',
+  backgroundColor: '#009688',
+  color: 'white',
+  textAlign: 'center',
+  padding: '5px 0px 5px 0px',
+  fontStyle: 'italic',
+  transform: 'rotate(-3.0deg)'
+}
+
+const readMoreBtn = {
+  outline: 'none',
+  background: '#009688',
+  color: 'white',
+  fontSize: '1.1rem',
+  fontFamily: 'inherit',
+  border: '1px transparent',
+  borderRadius: '10rem',
+  height: '50px',
+  width: '180px',
+  boxShadow: '0px 8px 15px rgba(0,0,0,0.15)',
+  display: 'inline'
+}
+const banner = {
+  width: '40%',
+  position: 'absolute',
+  zIndex: '-1',
+  transform: 'rotate(-30deg)',
+  // marginTop: '-5%',
+  marginLeft: '40%'
+}
+
+const homeSection = {
+  height: '88vh',
+  padding: '200px 15% 50px 15%',
+  boxSizing: 'border-box',
+  overflowX: 'hidden',
+  textAlign:'left'
+}
+const whiteLink = {
+  color: 'white',
+  textDecoration: 'None'
+}
+
+const aboutSection = {
+  height: '100vh',
+  lineHeight: '1px',
+  padding: '50px 15% 0px 15%',
+  textAlign: 'center',
+  boxSizing: 'border-box',
+  background: '#fafafa',
+  heading: {
+    color: 'black',
+    fontFamily: 'inherit',
+    fontWeight: '700'
+  },
+  text: {
+    fontSize: '25px',
+    fontWeight: '200',
+    lineHeight: '50px',
+    textAlign: 'center',
+  }
+
+}
+
+const footerSection = {
+  height: '200px',
+  textAlign: 'center',
+  fontWeight: '400'
+}
+const socialBox = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-evenly',
+  alignItems: 'center'
+}
+const last = {
+  fontWeight: '400',
+
 }
 
 
@@ -45,7 +161,62 @@ const IndexPage = () => {
           <li style={menuItem}>CONTACT</li>
         </ul>
       </div>
-    </main>
+      <div style={homeSection}>
+        <img style={banner} src="/logo.png"></img>
+        <h1 style={greeting}>Hello<img style={hiImage} src="/hi.png"></img>, <br></br>
+          <span>I'm <span style={name}>Bilal Alam</span></span>
+        </h1>
+        <h2 style={description}>A full-stack developer <span style={teal}>|</span> Computer Science graduate
+        <span style={teal}> |</span>  loves javascript <span style={teal}>|</span>  good at turning caffeine into code.</h2>
+        <button style={readMoreBtn}>ABOUT ME</button>
+      </div>
+      <div style={aboutSection}>
+        <h1 style={aboutSection.heading}>Education</h1>
+
+        <h2 style={aboutSection.text}>
+          <div>My Computer Science Journey started when I enrolled in <span style={highlight}>BS Computer Science</span> programme
+          at <span style={highlight}>University of Karachi</span> in <code>January, 2015</code>, from where I graduated
+          in <code>December 2018.</code></div>
+          <div>During my time there, I studied core academic subjects in Computer Science/STEM and a few electives</div>
+          <div>including <code>OOP, Data Structures, DBMS, Networking, Operating Systems, Parallel Computing, Computer Graphics with openGL, Text mining with python.</code></div>
+          <div>I built several <span style={highlight}>interesting projects</span></div>
+          <div>including an Inventory Management System <code>(java, JDBC, Mysql)</code></div>
+          <span style={teal}> | </span>Blogging site <code>(angularjs, php, mysql)</code>
+          <span style={teal}> | </span>MS Paint clone <code>(OpenGL, C++)</code><span style={teal}> | </span>
+          Pocket Accountant; Mobile App <code>(MEAN stack, ionic, speech recognition)</code>
+        </h2>
+        <h1 style={aboutSection.heading}>Career</h1>
+
+        <h2 style={aboutSection.text}>
+          <div> I have <span style={highlight}>4 years</span> of professional experience. I started while still undergrad, as a part-time <span style={highlight}>front-end developer</span>
+          where enterprise applications for 2 years, involving technologies like <code>
+              angularjs, angular 2+, jquery, javascript, serverless architecture, sql, no-sql, python, mongodb
+          </code>
+            <br></br>
+          Meanwhile I did <span style={highlight}>freelancing</span> too, I completed three web and one mobile apps for multiple clients
+          involving technologies like <code>MEAN stack</code></div>
+          <div>After graduation, I joined a renowned product based company as a full-time software engineer, where I worked on multiple
+            products and technologies like <code>Angular, StencilJs, Chrome extensions, MS word Add-ins, Apache Spark, Java Servlets</code>
+          </div>
+          <div> Currently, I am working as a <span style={highlight}>Full-stack developer</span> at <a target="_blank" href="https://www.venturedive.com"> VentureDive</a></div>
+        </h2>
+        <h1>&nbsp;</h1>
+        <a style={whiteLink} target="_blank" href="https://drive.google.com/file/d/1VB8Z195w5z8jn1MhjzNB6-KNI98O1-bb/view?usp=sharing"><button style={readMoreBtn}>RESUME</button></a>
+      </div>
+      <div style={footerSection}>
+        <h1 style={last}>Want me to solve a problem for you or just want to say hi? feel free to contact me ^_^ </h1>
+        <br />
+        <div style={socialBox}>
+          <a style={whiteLink} target="_blank" href="https://drive.google.com/file/d/1VB8Z195w5z8jn1MhjzNB6-KNI98O1-bb/view?usp=sharing"><button style={readMoreBtn}>Linkedin</button></a>
+          <a style={whiteLink} target="_blank" href="https://drive.google.com/file/d/1VB8Z195w5z8jn1MhjzNB6-KNI98O1-bb/view?usp=sharing"><button style={readMoreBtn}>Stackoverflow</button></a>
+          <a style={whiteLink} target="_blank" href="https://drive.google.com/file/d/1VB8Z195w5z8jn1MhjzNB6-KNI98O1-bb/view?usp=sharing"><button style={readMoreBtn}>Github</button></a>
+          <a style={whiteLink} target="_blank" href="https://drive.google.com/file/d/1VB8Z195w5z8jn1MhjzNB6-KNI98O1-bb/view?usp=sharing"><button style={readMoreBtn}>Twitter</button></a>
+          <a style={whiteLink} target="_blank" href="https://drive.google.com/file/d/1VB8Z195w5z8jn1MhjzNB6-KNI98O1-bb/view?usp=sharing"><button style={readMoreBtn}>Medium</button></a>
+          <a style={whiteLink} target="_blank" href="https://drive.google.com/file/d/1VB8Z195w5z8jn1MhjzNB6-KNI98O1-bb/view?usp=sharing"><button style={readMoreBtn}>Email</button></a>
+        </div>
+
+      </div>
+    </main >
   )
 }
 
